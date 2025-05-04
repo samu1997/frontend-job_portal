ef070587ae0503a2b63a16c1f1ebd81f6f4a1e9e
samarth udgiri <samarthudgiri@outlook.com>
2025-05-04 23:22:08 +0530

    all UI library removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,13 @@
 import { NgModule } from '@angular/core';
-import { registerLocaleData } from '@angular/common';
-import en from '@angular/common/locales/en';
-import { FormsModule } from '@angular/forms';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-
-import { provideNzI18n } from 'ng-zorro-antd/i18n';
-import { en_US } from 'ng-zorro-antd/i18n';
-import { StylingModule } from './shared/styling.module';
-
-registerLocaleData(en);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, SharedModule, StylingModule],
-  providers: [
-    provideClientHydration(withEventReplay()),
-    provideNzI18n(en_US),
-    provideAnimationsAsync(),
-    provideHttpClient(),
-  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [provideClientHydration(withEventReplay())],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/shared/styling.module.ts
+++ src/app/shared/styling.module.ts
@@ -1,46 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzIconModule } from 'ng-zorro-antd/icon';
-import { NzTypographyModule } from 'ng-zorro-antd/typography';
-import { NzDividerModule } from 'ng-zorro-antd/divider';
-import { NzFlexModule } from 'ng-zorro-antd/flex';
-import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzSpaceModule } from 'ng-zorro-antd/space';
-import { NzSplitterModule } from 'ng-zorro-antd/splitter';
-import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
-import { NzPaginationModule } from 'ng-zorro-antd/pagination';
-import { NzStepsModule } from 'ng-zorro-antd/steps';
-import { NzSpinModule } from 'ng-zorro-antd/spin';
-import { NzSkeletonModule } from 'ng-zorro-antd/skeleton';
-import { NzModalModule } from 'ng-zorro-antd/modal';
-import { NzDrawerModule } from 'ng-zorro-antd/drawer';
-import { NzAlertModule } from 'ng-zorro-antd/alert';
-import { NzTableModule } from 'ng-zorro-antd/table';
-
 @NgModule({
-  exports: [
-    NzTypographyModule,
-    NzButtonModule,
-    NzIconModule,
-    NzDividerModule,
-    NzFlexModule,
-    NzGridModule,
-    NzLayoutModule,
-    NzSpaceModule,
-    NzAlertModule,
-    NzSplitterModule,
-    NzDropDownModule,
-    NzMenuModule,
-    NzPaginationModule,
-    NzStepsModule,
-    NzSpinModule,
-    NzSkeletonModule,
-    NzModalModule,
-    NzDrawerModule,
-    NzTableModule,
-  ],
+  exports: [],
 })
 export class StylingModule {}
